4a8a0a75183bdfcfb1937fe530293590bcef67c9
Laiba Gohar <149240308+laibagohar923@users.noreply.github.com>
2024-07-09 12:20:11 -0700

    Update script.js

--- Expence tracker/script.js
+++ Expence tracker/script.js
@@ -1,19 +1,3 @@
-// const panels = document.querySelectorAll('.panel');
-
-// panels.forEach((panel) => {
-//     panel.addEventListener('click',()=> {
-//         removeActiveClasses();
-//         panel.classList.add('active');
-//     })
-// })
-
-// function removeActiveClasses(){
-//     panels.forEach(panel => {
-//         panel.classList.remove('active');
-//     })
-// }
-
-
 
 
 //1
